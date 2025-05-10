bb6b4c1baeaa41559cddb61a155ca6a91998ac65
la la <13687739+daedalus1215@users.noreply.github.com>
2025-05-10 15:44:31 -0400

    add-pass: stop the component rerender cycle

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import styles from "./HomePage.module.css";
 import { cn } from "@/lib/utils";
-import { PasswordTable } from "@/pages/HomePage/components/PasswordTable/PasswordTable";
+import { PasswordTable } from "./components/PasswordTable/PasswordTable";
 import { useState } from "react";
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
