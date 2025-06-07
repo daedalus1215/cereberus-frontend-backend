10966de80f4dffe5f7f609d0abaca5bee8c621f9
la la <13687739+daedalus1215@users.noreply.github.com>
2025-06-07 11:45:11 -0400

    add-pass: fix axios

--- frontend/src/api/axios.interceptor.ts
+++ frontend/src/api/axios.interceptor.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api/',  // We'll handle the full path in the interceptor
+  baseURL: '/',  // We'll handle the full path in the interceptor
   headers: {
     'Content-Type': 'application/json',
   },
