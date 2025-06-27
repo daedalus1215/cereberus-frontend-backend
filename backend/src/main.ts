54246ab7395d081aa3568cf2470f1dee125cc96c
la la <13687739+daedalus1215@users.noreply.github.com>
2025-06-26 23:35:26 -0400

    https-switch

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,27 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
+import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
 
-  // Set global prefix for all routes
-  app.setGlobalPrefix('api');
+  let httpsOptions: HttpsOptions | undefined;
+  if (process.env.NODE_ENV === 'development') {
+    httpsOptions = undefined;
+  } else {
+    httpsOptions = {
+      key: fs.readFileSync(process.env.SSL_KEY_PATH),
+      cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+    };
+  }
 
-  // Enable CORS
-  app.enableCors({
-    origin: true, // Allow all origins in development. For production, specify your frontend URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+    const app = await NestFactory.create(AppModule, { httpsOptions});
 
-  // Enable global validation
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Strip properties that do not have any decorators
-    forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
-    transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
-  }));
+    // Set global prefix for all routes
+    app.setGlobalPrefix('api');
 
-  await app.listen(3000);
+    // Enable CORS
+    app.enableCors({
+      origin: true, // Allow all origins in development. For production, specify your frontend URL
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+	    });
+
+    // Enable global validation
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true, // Strip properties that do not have any decorators
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
+      transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
+    }));
+
+
+    await app.listen(process.env.PORT); // Or 8443 if not running as root
 }
 bootstrap();
