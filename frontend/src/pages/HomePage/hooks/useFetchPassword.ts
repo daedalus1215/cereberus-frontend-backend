0c80afae4ac2adac27106af2997568b10a8e2495
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2026-04-14 22:36:11 -0400

    hotfix: resolve password change display Before, we would show stale password, if we revealed it, after we changed it.

--- frontend/src/pages/HomePage/hooks/useFetchPassword.ts
+++ frontend/src/pages/HomePage/hooks/useFetchPassword.ts
@@ -1,14 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchPassword } from "@/api/passwords";
 import { PasswordEntryResponse } from "../components/PasswordTable/types";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export const useFetchPassword = (objectToRevealId: string | null) => {
   const [revealedId, setRevealedId] = useState<string | null>(objectToRevealId);
 
+  const queryKey = useMemo(() => ["password", revealedId], [revealedId]);
+
+
   const { data: revealedPassword, isLoading: isLoadingPassword } =
     useQuery<PasswordEntryResponse>({
-      queryKey: ["password", revealedId],
+      queryKey,
       queryFn: () => fetchPassword(revealedId!),
       enabled: !!revealedId,
       staleTime: 0,
